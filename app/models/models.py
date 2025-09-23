fed879d90d38c128097fe59d2aef325feaf762ed
Long Nguyen <nguyenkimlog59@gmail.com>
2025-09-23 22:45:06 +0700

    fix: missing dependency

--- app/models/models.py
+++ app/models/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String, T
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
-from database import Base
+from app.database import Base
 
 
 class Company(Base):
